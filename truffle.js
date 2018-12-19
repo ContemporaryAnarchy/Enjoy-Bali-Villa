28d2f9b8d186c08de00d9db08bb242c23068664a
Etienne Dusseault <etienne.dusseault@gmail.com>
2018-12-19 09:43:32 +0800

    got rid of keys

--- truffle.js
+++ truffle.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider('bargain radar abuse among segment talent floor rice jazz banana chair midnight', "https://ropsten.infura.io/92605767b61346d28be78407e0e7fc62", 0, 10)
+        return new HDWalletProvider(process.env.ROPSTEN_MNEMONIC, `https://ropsten.infura.io/${process.env.INFURA_API_KEY}`, 0, 10)
       },
       network_id: 3,
       gasPrice: 25000000000
